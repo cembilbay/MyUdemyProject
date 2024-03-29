68b3c3388ef5d59d7961ab04b94eccb0946d1e16
Cem Arda BİLBAY <200601053@bakircay.edu.tr>
2024-03-29 13:49:48 +0200

    added controllers and layout

---
+++ ApiConsume/HotelProject.WebApi/Controllers/ServiceController.cs
@@ -0,0 +1,51 @@
+﻿using HotelProject.Businesslayer.Abstract;
+using HotelProject.EntityLayer.Concrate;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        private readonly IServicesService _servicesService;
+
+        public ServiceController(IServicesService servicesService)
+        {
+            _servicesService = servicesService;
+        }
+
+        [HttpGet]
+        public IActionResult ServiceList()
+        {
+            var values = _servicesService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddService(Service service)
+        {
+            _servicesService.TInsert(service);
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteService(int id)
+        {
+            var values = _servicesService.TGetById(id);
+            _servicesService.TDelete(values);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateService(Service Service)
+        {
+            _servicesService.TUpdate(Service);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetService(int id)
+        {
+            var values = _servicesService.TGetById(id);
+            return Ok(values);
+        }
+    }
+}
--- ApiConsume/HotelProject.WebApi/Program.cs
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -1,7 +1,23 @@
+using HotelProject.Businesslayer.Abstract;
+using HotelProject.Businesslayer.Concrate;
+using HotelProject.DataAcessLayer.Abstract;
+using HotelProject.DataAcessLayer.Concrate;
+using HotelProject.DataAcessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<Context>();
+builder.Services.AddScoped<IStaffDal,EfStaffDal>();
+builder.Services.AddScoped<IStaffService,StaffManager>();
+builder.Services.AddScoped<IServicesDal,EfServiceDal>();
+builder.Services.AddScoped<IServicesService,ServiceManager>();
+builder.Services.AddScoped<IRoomDal,EfRoomDal>();
+builder.Services.AddScoped<IRoomService,RoomManager>();
+builder.Services.AddScoped<ISubscribeDal,EfSubscribeDal>();
+builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
+builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
